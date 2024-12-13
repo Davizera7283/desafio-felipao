7c4753cd69c473506ac403978b29a7a625a6151e
Davi Henrique <daviworldgo@gmail.com>
2024-12-13 17:48:02 -0300

    Atualização 2

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-adasda
+let nomeHeroi = "Davizera"
+let nivelHeroi = 23233434
+
+console.log("Olá " + nomeHeroi)
+
+if(nivelHeroi <= 1000){
+    console.log("Você esta no rank  Ferro")
+}else if(nivelHeroi >= 1001 && nivelHeroi <= 2000){
+    console.log("Você esta no rank Bronze")
+}else if(nivelHeroi >= 2001 && nivelHeroi <= 5000){
+    console.log("Você esta no rank Prata")
+}else if(nivelHeroi >= 5001 && nivelHeroi <=7000){
+    console.log("Você esta no rank Ouro")
+}else if(nivelHeroi >= 7001 && nivelHeroi <= 8000){
+    console.log("Você esta no rank Platina")
+}else if(nivelHeroi >= 8001 && nivelHeroi <= 9000){
+    console.log("Você esta no rank Ascendente")
+}else if(nivelHeroi >= 9001 && nivelHeroi <= 10000){
+    console.log("Você esta no rank Imortal")
+}else{
+    console.log("Você esta no rank Radiante")
+} 
+
+
