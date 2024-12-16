0266ed95ab7d5be0a679b108f1c0abdb32cdffa5
Davi Henrique <daviworldgo@gmail.com>
2024-12-16 08:44:11 -0300

    Atualização Final

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let nomeHeroi = "Davizera"
-let nivelHeroi = 23233434
+let nivelHeroi = 123
 let nivel
-console.log("Olá " + nomeHeroi)
+
 
 if(nivelHeroi <= 1000){
     nivel = "Ferro"
@@ -10,15 +10,15 @@ if(nivelHeroi <= 1000){
 }else if(nivelHeroi >= 2001 && nivelHeroi <= 5000){
     nivel = "Prata"
 }else if(nivelHeroi >= 5001 && nivelHeroi <=7000){
-    console.log("Você esta no rank Ouro")
+    nivel = "Ouro"
 }else if(nivelHeroi >= 7001 && nivelHeroi <= 8000){
-    console.log("Você esta no rank Platina")
+    nivel = "Platina"
 }else if(nivelHeroi >= 8001 && nivelHeroi <= 9000){
-    console.log("Você esta no rank Ascendente")
+    nivel = "Ascendente"
 }else if(nivelHeroi >= 9001 && nivelHeroi <= 10000){
-    console.log("Você esta no rank Imortal")
+    nivel = "Imortal"
 }else{
-    console.log("Você esta no rank Radiante")
+    nivel = "Radiante" 
 } 
 
-
+console.log("O " + nomeHeroi + " tem " + nivelHeroi + " de XP e está no rank " + nivel)
